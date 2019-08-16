c591b83a54c4b575478c3719b4ee45cf800e0860
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2019-08-16 19:19:15 +0900

    wip

--- src/module.tsx
+++ src/module.tsx
@@ -1,8 +1,16 @@
 import React, { PureComponent } from 'react';
 import { PanelProps, PanelPlugin } from '@grafana/ui';
+import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
 
 export class MyPanel extends PureComponent<PanelProps> {
   render() {
+    //public/app/features/dashboard/dashgrid/DashboardPanel.tsx
+    //    this.loadPlugin(plugin.id);
+
+    const dashboard = getDashboardSrv().getCurrent();
+    const panel = dashboard.getPanelById('2');
+    console.log(panel);
+
     return <div>Hello from my panel</div>;
   }
 }
